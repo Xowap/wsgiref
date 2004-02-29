6020a0bbc40ea8206eaf0ed64d98433de3a14192
pje <pje@571e12c6-e1fa-0310-aee7-ff1267fa46bd>
2004-02-29 23:03:07 +0000

    Update PyProtocols to use 'setuptools', and clean up some leftover junk.
    git-svn-id: svn://svn.eby-sarna.com/svnroot/wsgiref@232 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- setuptools/dist.py
+++ setuptools/dist.py
@@ -10,15 +10,9 @@ class Distribution(_Distribution):
     # 'test' command
     test_module = None
 
-    # 'install_pkg_data' command
-    package_data = ()
-
-
     def __init__ (self, attrs=None):
+        self.package_data = {}
         _Distribution.__init__(self,attrs)
         self.cmdclass.setdefault('build_py',build_py)
         # XXX self.cmdclass.setdefault('build',build)
-
-    def has_pkg_data(self):
-        return self.package_data
 
